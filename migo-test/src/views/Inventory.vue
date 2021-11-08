6027e98ed1a53a4cf3ab9c037afa08c6dd272c1c
weily10 <w3ily10@gmail.com>
2021-11-09 01:01:40 +0800

    expansion row done

--- migo-test/src/views/Inventory.vue
+++ migo-test/src/views/Inventory.vue
@@ -36,20 +36,46 @@
           </tr>
         </thead>
         <tbody>
-          <tr v-for="(item,index) in titles" :key="index">
-            <td class="text-start" >{{item.title_id}}</td>
-            <td class="text-title" align="left">{{item.title_name}}</td>
-            <td class="text-start">{{item.content_type}}</td>
-            <td class="text-start">{{item.seasons.length}}</td>
-            <td class="text-start">{{item.episode_count}}</td>
-            <td class="text-start">{{item.publish_timestamp}}</td>
-            <td class="text-start">
-              <label class="switch">
-                <input type="checkbox" />
-                <span class="slider round"></span>
-              </label>
-            </td>
-          </tr>
+          <template v-for="(item, index) in titles">
+            <tr @click="toggle(item.title_id)" :key="index">
+              <td class="text-start">{{ item.title_id }}</td>
+              <td class="text-title" align="left">{{ item.title_name }}</td>
+              <td class="text-start">
+                {{ item.content_type }}
+              </td>
+
+              <td class="text-start">{{ item.seasons.length }}</td>
+              <td class="text-start">{{ item.episode_count }}</td>
+              <td class="text-start">{{ item.publish_timestamp }}</td>
+              <td class="text-start">
+                <label class="switch">
+                  <input type="checkbox" />
+                  <span class="slider round"></span>
+                </label>
+              </td>
+            </tr>
+            <template v-for="(season,index) in item.seasons">
+              <tr
+                v-if="
+                  opened.includes(item.title_id) &&
+                  item.content_type === 'Series'
+                "
+                :key="season.season_id"
+              >
+                <td class="text-start1">
+                  {{ season.season_id }}
+                </td>
+                <td class="text-start1">
+                  Season  {{index+1}}
+                </td>
+
+                <td class="text-start1">Season</td>
+                <td class="text-start1">
+                  {{ season.season_name }}
+                </td>
+              </tr>
+            </template>
+          </template>
         </tbody>
       </table>
     </section>
@@ -61,8 +87,19 @@ import axios from "axios";
 export default {
   data() {
     return {
+      opened: [],
       titles: [],
     };
+  },
+  methods: {
+    toggle(id) {
+      const index = this.opened.indexOf(id);
+      if (index > -1) {
+        this.opened.splice(index, 1);
+      } else {
+        this.opened.push(id);
+      }
+    },
   },
   mounted() {
     axios
@@ -76,28 +113,28 @@ export default {
 };
 </script>
 <style scoped>
-.text-start{
+.text-start {
   font-weight: 400;
   font-size: 12px;
   line-height: 14px;
 }
-.text-title{
+.text-title {
   font-weight: 600;
   font-size: 12px;
   line-height: 14px;
 }
 table {
   border-collapse: collapse;
-  border: thin solid rgba(0,0,0,.12);
+  border: thin solid rgba(0, 0, 0, 0.12);
 }
-tr{
-  height:48px;
+tr {
+  height: 48px;
   padding: 20px;
-  border-bottom: thin solid rgba(0,0,0,.12);
+  border-bottom: thin solid rgba(0, 0, 0, 0.12);
 }
-th{
+th {
   background: rgba(32, 32, 32, 0.05);
-  padding:10px
+  padding: 10px;
 }
 .table-1 {
   width: 100%;
@@ -132,7 +169,7 @@ th{
   position: relative;
   display: inline-block;
   width: 47px;
-  height:21px;
+  height: 21px;
 }
 
 /* Hide default HTML checkbox */
@@ -150,7 +187,7 @@ th{
   left: 0;
   right: 0;
   bottom: 0;
-  background-color:#B2B2B2;
+  background-color: #b2b2b2;
   -webkit-transition: 0.4s;
   transition: 0.4s;
 }
@@ -168,11 +205,11 @@ th{
 }
 
 input:checked + .slider {
-  background-color: #08AA5E;;
+  background-color: #08aa5e;
 }
 
 input:focus + .slider {
-  box-shadow: 0 0 1px #08AA5E;;
+  box-shadow: 0 0 1px #08aa5e;
 }
 
 input:checked + .slider:before {
